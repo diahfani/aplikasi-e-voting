414b3069e8f2f3e1f1b2824d74a41c74c5a87f21
diahfani <49426318+diahfani@users.noreply.github.com>
2021-01-12 14:13:51 +0700

    Add files via upload

---
+++ uas_pbd/src/uas/model/modelVoter.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas.model;
+
+/**
+ *
+ * @author Admin
+ */
+public class modelVoter {
+    private String namaVoterModel;
+    private String npmVoterModel;
+    private byte[] ktmVoterModel;
+
+    public String getNamaVoterModel() {
+        return namaVoterModel;
+    }
+
+    public void setNamaVoterModel(String namaVoterModel) {
+        this.namaVoterModel = namaVoterModel;
+    }
+
+    public String getNpmVoterModel() {
+        return npmVoterModel;
+    }
+
+    public void setNpmVoterModel(String npmVoterModel) {
+        this.npmVoterModel = npmVoterModel;
+    }
+
+    public byte[] getKtmVoterModel() {
+        return ktmVoterModel;
+    }
+
+    public void setKtmVoterModel(byte[] ktmVoterModel) {
+        this.ktmVoterModel = ktmVoterModel;
+    }
+    
+    
+}
